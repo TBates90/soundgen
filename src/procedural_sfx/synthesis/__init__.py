b1f79db33ff5b9e8c78af6e7ef1e43a3535820cf
Tom Bates <69461730+TBates90@users.noreply.github.com>
2026-09-01 04:26:31 +0100

    feat: export synthesis generators

--- src/procedural_sfx/synthesis/__init__.py
+++ src/procedural_sfx/synthesis/__init__.py
@@ -1,1 +1,21 @@
 """Procedural audio synthesis components."""
+
+from procedural_sfx.synthesis.generators import (
+    Generator,
+    GeneratorFactory,
+    SawGenerator,
+    SineGenerator,
+    SquareGenerator,
+    TriangleGenerator,
+    WhiteNoiseGenerator,
+)
+
+__all__ = [
+    "Generator",
+    "GeneratorFactory",
+    "SawGenerator",
+    "SineGenerator",
+    "SquareGenerator",
+    "TriangleGenerator",
+    "WhiteNoiseGenerator",
+]
